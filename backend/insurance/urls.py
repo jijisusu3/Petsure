1c36f5d5a1e598d813008eecf2dc3f789bd2f267
zyyyyysoo <newsty1107@gmail.com>
2022-09-16 16:17:13 +0900

    Feat: add API urls

--- backend/insurance/urls.py
+++ backend/insurance/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from . import views
 
 app_name = 'insurance'
 
 urlpatterns = [
-    
+    path('breed/', views.breed),
+    path('breed/<species>/<name>/', views.breed_search),
+    path('insurance/basic/', views.basic),
+    path('insurance/detail/', views.detail),
+    path('insurance/choice/', views.choice),
+    path('insurance/servey/', views.servey),
+    path('disease/', views.disease),
+    path('disease/<int:desease_id>/', views.disease_detail),
 ]
