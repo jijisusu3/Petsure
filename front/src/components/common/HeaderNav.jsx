793268c2624dce168dc6043e3f264f9f62f6a349
sungjae <lsuksa1@naver.com>
2022-09-20 10:48:42 +0900

    feat: headerNav

--- front/src/components/common/HeaderNav.jsx
+++ front/src/components/common/HeaderNav.jsx
@@ -1,47 +1,80 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-
-import MenuBtn from './MenuBtn';
-import classes from './HeaderNav.module.css';
-import Button from './Button';
-
-import DropdownMenu from './DropdownMenu';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function HeaderNav() {
-  const state = useSelector(state => state.user);
-  // const state = {
-  //   id: 'test',
-  // }
-  const [showMenu, setShowMenu] = useState(false);
-
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const globalClickHandler = ({ target }) => {
-    if (!target.classList.contains('click-blocked')) setShowMenu(!showMenu);
-  };
-  const handleClick = () => setShowMenu(!showMenu);
-
   return (
-    <nav className={`${classes.headerNav} ${state.id ? classes.auth : ''}`}>
-      <div className={classes.navWrapper}>
-        <div className={classes.HeaderNav_link_btns}>
-          {state.id && (
-            <div className={`${classes.HeaderNav_link_btns} ${classes.hover_color}`}>
-              <MenuBtn text="홈" link="/meeting" />
-
-              <MenuBtn
-                className={classes.HeaderNav_link_focus}
-                text="랜덤매칭"
-                link="/meeting?rematching=true"
-              />
-            </div>
-          )}
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="white" variant="white">
+      <Container>
+        <Navbar.Brand href="#landing">
+          <img
+            src="petsureLogo.png"
+            width="16"
+            height="16"
+            className="d-inline-block align-top"
+            alt=""
+          />
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">펫 보험</Nav.Link>
+          <Nav.Link href="#features">질병상식</Nav.Link>
+          <Nav.Link href="#pricing">ABOUT US</Nav.Link>
+        </Nav>
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <a href="#guide">가이드</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
 export default HeaderNav;
+// import { useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+
+// import MenuBtn from './MenuBtn';
+// import classes from './HeaderNav.module.css';
+// import Button from './Button';
+
+// import DropdownMenu from './DropdownMenu';
+
+// function HeaderNav() {
+//   const state = useSelector(state => state.user);
+//   // const state = {
+//   //   id: 'test',
+//   // }
+//   const [showMenu, setShowMenu] = useState(false);
+
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+
+//   const globalClickHandler = ({ target }) => {
+//     if (!target.classList.contains('click-blocked')) setShowMenu(!showMenu);
+//   };
+//   const handleClick = () => setShowMenu(!showMenu);
+
+//   return (
+//     <nav className={`${classes.headerNav} ${state.id ? classes.auth : ''}`}>
+//       <div className={classes.navWrapper}>
+//         <div className={classes.HeaderNav_link_btns}>
+//           {state.id && (
+//             <div className={`${classes.HeaderNav_link_btns} ${classes.hover_color}`}>
+//               <MenuBtn text="홈" link="/meeting" />
+
+//               <MenuBtn
+//                 className={classes.HeaderNav_link_focus}
+//                 text="랜덤매칭"
+//                 link="/meeting?rematching=true"
+//               />
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default HeaderNav;
