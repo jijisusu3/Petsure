7e0bdb77c0f30d29a853607afbe4a4bac1c45d90
이성재 <lsuksa1@naver.com>
2022-09-27 12:10:38 +0900

    ✨ feat(insurecard): insurecard, headernav

---
+++ front/src/components/detail-result/InsureCard.jsx
@@ -0,0 +1,36 @@
+import { SureScore } from '../common/SureScore';
+import Sheet from '../common/Sheet';
+import React from 'react';
+import classes from './InsureCard.module.css';
+import Button from 'react-bootstrap/Button';
+
+const data = 70.35;
+
+export function InsureCard() {
+  return (
+    <div className={classes.insureCard}>
+      <div>
+        <img
+          className={classes.insure_logo}
+          alt="insure"
+          src="https://blog.kakaocdn.net/dn/lmjhi/btqB8oDRuQQ/JfqZ41bVWFDcn1a1kDk9m1/img.jpg"
+        />
+      </div>
+      <div>
+        <h2>보험이름들어가면될듯</h2>
+        <h5>보장내용</h5>
+        <Sheet size="badge">슬개골</Sheet>
+      </div>
+      <div className={classes.sureScore}>
+        <h3>Sure점수</h3>
+        <SureScore value={data} />
+        <h4>평균 70.34</h4>
+      </div>
+      <div>
+        <h5>월 19,000원</h5>
+        <Button className={classes.detailButton}>상세보기</Button>
+        <Button className={classes.compareButton}>비교하기</Button>
+      </div>
+    </div>
+  );
+}
