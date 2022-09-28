60691aea08b9c48c6e1748d05d2f8a7d27a1f0ef
ys <ysltub@gmail.com>
2022-09-29 02:24:47 +0900

    refactor: 💡 main page contents complete

---
+++ front/src/components/landing/LandingFour.jsx
@@ -0,0 +1,12 @@
+import classes from './LandingFour.module.css';
+
+export default function LandingThree() {
+  return (
+    <div className={classes.landingFour}>
+      <br />
+      <br />
+      <h1>그럼, 바로 상세 추천을 받아볼까요?</h1>
+      <button style={{ marginTop: '30px' }}>맞춤 펫보험 알아보기</button>
+    </div>
+  );
+}
--- front/src/components/landing/LandingThree.jsx
+++ front/src/components/landing/LandingThree.jsx
@@ -1,13 +1,23 @@
-import { margin } from '@mui/system';
 import classes from './LandingThree.module.css';
 
 export default function LandingThree() {
   return (
-    <div className={classes.landingThree}>
+    <div className={`${classes.landingThree}`}>
       <h1>우리 아이에게 딱 맞는 보험</h1>
-      <p>펫슈어가 확실하게 찾아드릴게요!</p>
-      <div className={classes.boxes}>
+      <p>펫슈어가 확실하게 찾아드릴게요! ☝</p>
+      <div className={`${classes.boxes}`}>
         <div className={classes.box1}>
+          <img src="img/detailselect.svg" alt="" />
+          <div className={classes.box}>
+            <p className={`${classes.boxtitle}`}>보장 선호도를 알려주세요!</p>
+            <p style={{ fontSize: '15px', marginTop: '20px', color: '#838383' }}>
+              선호도에 맞춰
+              <br /> 당신에게 적합한 맞춤 보험을
+              <br /> 추천해드릴게요!
+            </p>
+          </div>
+        </div>
+        <div className={classes.box2}>
           <img src="img/SureScore.svg" alt="" />
           <div className={classes.box}>
             <p className={classes.boxtitle}>슈어 점수를 확인하세요!</p>
@@ -18,12 +28,16 @@ export default function LandingThree() {
             </p>
           </div>
         </div>
-        <div className={classes.box2}>
-          <img src="img/detailselect.png" alt="" />
-          <div className={classes.box}>하하하하2</div>
-        </div>
-        <div>
-          <div className={classes.box}>하하하하2</div>
+        <div className={classes.box3}>
+          <img src="img/scale.svg" alt="" />
+          <div className={classes.box}>
+            <p className={classes.boxtitle}>보험을 비교해보세요!</p>
+            <p style={{ fontSize: '15px', marginTop: '20px', color: '#838383' }}>
+              추천받은 보험들을
+              <br /> 동시에, 한번에, 여러개
+              <br /> 비교해보세요!
+            </p>
+          </div>
         </div>
       </div>
     </div>
