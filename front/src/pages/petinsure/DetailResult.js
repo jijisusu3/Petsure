f88d87c20b9fe200b8f77741040a73d68fc6f3ed
이성재 <lsuksa1@naver.com>
2022-10-04 13:50:48 +0900

    🐛 fix(pages/petinsure/detailresult): detailResultapi

--- front/src/pages/petinsure/DetailResult.js
+++ front/src/pages/petinsure/DetailResult.js
@@ -2,39 +2,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DataComparison from '../../components/detail-result/InsureCard';
+import { useLocation } from 'react-router';
 
 export function DetailResultPage() {
   const [sDatas, setSDatas] = useState([]);
   const [pDatas, setPDatas] = useState([]);
   const [cDatas, setCDatas] = useState([]);
   const [user, setUser] = useState([]);
+  const { state } = useLocation();
   useEffect(() => {
-    // dispatch(prdActions.getPostDB());
-    axios
-      .post('/api/insurance/detail/', {
-        breed: 31,
-        animal_name: '이봉봉',
-        species: 1,
-        animal_birth: 2,
-        hospitalization: 4,
-        outpatient: 3,
-        operation: 1,
-        patella: 4,
-        skin_disease: 3,
-        dental: 2,
-        urinary: 1,
-        liability: 1,
-      })
-      .then(response => {
-        console.log(response);
-        setSDatas(response.data.sure_ranking);
-        setPDatas(response.data.price_ranking);
-        setCDatas(response.data.cover_ranking);
-        setUser(response.data.detail_user);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    setSDatas(state.sure_ranking);
+    setPDatas(state.price_ranking);
+    setCDatas(state.cover_ranking);
+    setUser(state.detail_user);
   }, []);
+
   return <DataComparison sDatas={sDatas} pDatas={pDatas} cDatas={cDatas} user={user} />;
 }
